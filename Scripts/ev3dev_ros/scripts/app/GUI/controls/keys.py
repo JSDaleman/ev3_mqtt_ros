d2d4d69741a9c9475989ed182b49542f8d9bde53
JSDaleman <jdaleman@unal.edu.co>
2025-01-31 13:15:40 -0500

    Se completo la clase de keys differencial para funcionamiento de la GUI

--- Scripts/ev3dev_ros/scripts/app/GUI/controls/keys.py
+++ Scripts/ev3dev_ros/scripts/app/GUI/controls/keys.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import app.mqtt.messages as messages
+
 class ControlKeys():
       
       def __init__(self, master, mqtt_client):
@@ -10,9 +12,12 @@ class DifferentialControlKeys(ControlKeys):
       
     def __init__(self, master, mqtt_client):
         super().__init__(master, mqtt_client)
+
         self.right_speed_entry = None
         self.left_speed_entry = None
         self.angle_value_label = None
+
+        self.entries = [self.right_speed_entry, self.left_speed_entry]
 
     def set_right_speed_entry(self, right_speed_entry):
         self.right_speed_entry = right_speed_entry
@@ -22,20 +27,102 @@ class DifferentialControlKeys(ControlKeys):
     
     def set_angle_value_label(self, angle_value_label):
         self.angle_value_label = angle_value_label
-    
-    def manejar_tecla_right(root, event, entry):
-        if root.focus_get() == entry:
-            # Comportamiento cuando el Entry tiene el enfoque
-            print("Cursor moviéndose a la derecha dentro del Entry")
-            return
-        else:
-            # Comportamiento cuando el Entry NO tiene el enfoque
-            print("Hola mundo (tecla Right fuera del Entry)")
-            return "break"  # Bloquea el comportamiento predeterminado de la tecla
 
-    def quitar_foco(root, event, entry):
+    def remove_focus(self, event):
         # Si el clic no es sobre el Entry, quita el enfoque
-        if event.widget != entry:
-            root.focus()
+        if event.widget not in [self.right_speed_entry, self.left_speed_entry]:
+            self.master.focus()
+
+    def block_keys(self):
+
+        focused_widget = self.master.focus_get()
+
+        if focused_widget in [self.right_speed_entry, self.left_speed_entry]:
+            # Comportamiento cuando el Entry tiene el enfoque
+            return True
+        
+        return False
+
+    def control_key_up(self, event):
+        try:
+            value_right = int(self.right_speed_entry.get())
+            value_left = int(self.left_speed_entry.get())
+            speed = max(value_right, value_left)
+            messages.send_message_movtank(self.mqtt_client, speed, speed, "Forward key")
+
+        except ValueError:
+            print("Please enter a valid number")
+
+    def control_key_left(self, event):
+
+        if self.block_keys():
+            return
+        
+        # Comportamiento cuando el Entry NO tiene el enfoque
+        try:
+            value_right = int(self.right_speed_entry.get())
+            value_left = int(self.left_speed_entry.get())
+
+            if value_left == value_right:
+                messages.send_message_movtank(self.mqtt_client, -1*value_left, 1*value_right, "Left key")
+            else:
+                speed = min(value_right, value_left)
+                messages.send_message_movtank(self.mqtt_client, -1*speed, 1*speed, "Left key")
+
+        except ValueError:
+            print("Please enter a valid number")
+
+        return "break"  # Bloquea el comportamiento predeterminado de la tecla
+
+
+    def control_key_right(self, event):
+
+        if self.block_keys():
+            return
+        
+        # Comportamiento cuando el Entry NO tiene el enfoque
+        try:
+            value_right = int(self.right_speed_entry.get())
+            value_left = int(self.left_speed_entry.get())
+
+            if value_left == value_right:
+                messages.send_message_movtank(self.mqtt_client, 1*value_left, -1*value_right, "Right key")
+            else:
+                speed = min(value_right, value_left)
+                messages.send_message_movtank(self.mqtt_client, 1*speed, -1*speed, "Right key")
+
+        except ValueError:
+            print("Please enter a valid number")
+
+        return "break"  # Bloquea el comportamiento predeterminado de la tecla
+    
+    def control_key_down(self, event):
+        try:
+            value_right = int(self.right_speed_entry.get())
+            value_left = int(self.left_speed_entry.get())
+            speed = min(value_right, value_left)
+            messages.send_message_movtank(self.mqtt_client, -1*speed, -1*speed, "Back key")
+
+        except ValueError:
+            print("Please enter a valid number")
+
+    def set_keys_control(self):
+
+        self.master.bind("<Button-1>", lambda event: self.remove_focus(event))
+
+        self.master.bind('<Up>', lambda event: self.control_key_up(event))
+        self.master.bind('<Left>', lambda event: self.control_key_left(event))
+        self.master.bind('<Right>', lambda event: self.control_key_right(event))
+        self.master.bind('<Down>', lambda event: self.control_key_down(event))
+
+        self.master.bind('<a>', lambda event: messages.send_message_special(self.mqtt_client, "Angle", "angle key"))
+        self.master.bind('<space>', lambda event: messages.send_message_special(self.mqtt_client, "Stop", "Stop key"))
+        self.master.bind('<u>', lambda event: messages.send_message_special(self.mqtt_client, "arm_up", "Up key"))
+        self.master.bind('<j>', lambda event: messages.send_message_special(self.mqtt_client, "arm_down", "Down key"))
+        self.master.bind('<q>', lambda event: messages.send_message_special(self.mqtt_client, "Quit", "Quit key"))
+        self.master.bind('<e>', lambda event: exit())
+        
+
+        
 
     
--- Scripts/ev3dev_ros/scripts/app/GUI/views/frames/differencial_frame.py
+++ Scripts/ev3dev_ros/scripts/app/GUI/views/frames/differencial_frame.py
@@ -4,6 +4,7 @@ import tkinter
 import app.mqtt.messages as messages
 import app.gui.views.frames.frame as frame
 import app.gui.controls.buttons as buttons_controls
+import app.gui.controls.keys as keys_controls
 
 class DifferencialFrame(frame.BaseFrame):
 
@@ -11,6 +12,7 @@ class DifferencialFrame(frame.BaseFrame):
         super().__init__(master, mqtt_client, delegate, padding)
 
         self.buttons_controls = buttons_controls.DifferentialControlButtons(self.master, self.mqtt_client)
+        self.keys_controls = keys_controls.DifferentialControlKeys(self.master, self.mqtt_client)
 
         self.create_widgets()
 
@@ -18,25 +20,7 @@ class DifferencialFrame(frame.BaseFrame):
         self.style_labels()
         self.style_entries()
 
-        self.master.bind('<a>', lambda event: messages.send_message_special(self.mqtt_client, "Angle", "angle key"))
-
-        self.master.bind('<Up>', lambda event: messages.send_message_movtank(self.mqtt_client, int(self.left_speed_entry.get()), int(self.right_speed_entry.get()), "Forward key"))
-
-        self.master.bind('<Left>', lambda event: messages.send_message_movtank(self.mqtt_client, (-1/2)*int(self.left_speed_entry.get()), (1/2)*int(self.right_speed_entry.get()), "Left key"))
-
-        self.master.bind('<space>', lambda event: messages.send_message_special(self.mqtt_client, "Stop", "Stop key"))
-
-        self.master.bind('<Right>', lambda event: messages.send_message_movtank(self.mqtt_client, (1/2)*int(self.left_speed_entry.get()), (-1/2)*int(self.right_speed_entry.get()), "Right key"))
-
-        self.master.bind('<Down>', lambda event: messages.send_message_movtank(self.mqtt_client, (-1)*int(self.left_speed_entry.get()), (-1)*int(self.right_speed_entry.get()), "Back key"))
-
-        self.master.bind('<u>', lambda event: messages.send_message_special(self.mqtt_client, "arm_up", "Up key"))
-
-        self.master.bind('<j>', lambda event: messages.send_message_special(self.mqtt_client, "arm_down", "Down key"))
-
-        self.master.bind('<q>', lambda event: messages.send_message_special(self.mqtt_client, "Quit", "Quit key"))
-
-        self.master.bind('<e>', lambda event: exit())
+        self.keys_controls.set_keys_control()
   
     def create_widgets(self):
         self.grid()
@@ -47,18 +31,21 @@ class DifferencialFrame(frame.BaseFrame):
         self.left_speed_label = self.create_label("Left", 0, 0)
         self.left_speed_entry = self.create_entry(1, 0, text="100", justify=tkinter.LEFT)
         self.buttons_controls.set_left_speed_entry(self.left_speed_entry)
+        self.keys_controls.set_left_speed_entry(self.left_speed_entry)
 
         """Creación de cuadro de texto para obtener valor de velocidad
         del motor derecho y etiqueta descritiva para el usuario"""
         self.right_speed_label = self.create_label("Right", 0, 2)
         self.right_speed_entry = self.create_entry(1, 2, text="100", justify=tkinter.RIGHT)
         self.buttons_controls.set_right_speed_entry(self.right_speed_entry)
+        self.keys_controls.set_right_speed_entry(self.right_speed_entry)
 
         """Creación de label para presnetar el valor de ángulo recibido y
         declaración en el objeto my_delagate del label que recibe dicho mensaje"""
         self.angle_label = self.create_label("Angle", 0, 3)
         self.angle_value_label = self.create_label("0", 1, 3)
         self.buttons_controls.set_angle_value_label(self.angle_value_label)
+        self.keys_controls.set_angle_value_label(self.angle_value_label)
         self.delegate.setlabel(self.angle_value_label)
 
         """Creación de los botones"""
@@ -72,7 +59,6 @@ class DifferencialFrame(frame.BaseFrame):
         self.down_button = self.create_button("Down", 6, 0, self.buttons_controls.send_down_message)
         self.q_button = self.create_button("Quit", 5, 2, self.buttons_controls.send_quit_message)
         self.e_button = self.create_button("Exit", 6, 2, self.buttons_controls.send_exit)
-
 
 
 
